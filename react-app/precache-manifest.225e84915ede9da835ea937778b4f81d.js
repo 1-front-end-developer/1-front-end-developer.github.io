0772a10ca98acea283a7d285ba24806b514375b1
1-front-end-developer <onefamily2@yandex.ru>
2020-02-29 17:17:14 +0400

    add folder react-app
    add folder react-app

---
+++ react-app/precache-manifest.225e84915ede9da835ea937778b4f81d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879b015ad33b985794f2c0cfceae11fb",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "50b0a4ab6bff37f735bb",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42d76a7af0ec991e0ac8",
+    "url": "/react-app/static/js/2.dc92a598.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app/static/js/2.dc92a598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b0a4ab6bff37f735bb",
+    "url": "/react-app/static/js/main.8b1def87.chunk.js"
+  },
+  {
+    "revision": "fee9a54793c47d9a47a4",
+    "url": "/react-app/static/js/runtime-main.ff43e42a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
